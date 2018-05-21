d17dca8d56f7554275aaed3ad2051b49d6403f94
golasowska <annagolasowska@gmail.com>
2018-05-21 10:38:32 +0200

    added content

--- src/components/QuickGuide.js
+++ src/components/QuickGuide.js
@@ -1,43 +1,53 @@
 import React, { Component } from "react";
+import { Document, Page } from "react-pdf";
 
 import Navigation from "../containers/Navigation";
 import Footer from "../containers//Footer";
 
 import logo_white from "../assets/images/bbod_white.svg";
 
-import qg1 from "../assets/images/quick-guide/qg-1.png";
-import qg2 from "../assets/images/quick-guide/qg-2.png";
-import qg3 from "../assets/images/quick-guide/qg-3.png";
-import qg4 from "../assets/images/quick-guide/qg-4.png";
+import qg from "../assets/images/quick-guide/BBOD.pdf";
 
 export default class QuickGuide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numPages: null,
+      pageNumber: 1
+    };
+  }
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
+  };
   render() {
+    const { pageNumber, numPages } = this.state;
     return (
       <div>
         <Navigation />
 
-        <div className="row mt-5">
-          <div className="col text-center">
-            <img src={qg1} className="img-fluid" alt="quick-guide" />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col text-center">
-            <img src={qg2} className="img-fluid" alt="quick-guide" />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col text-center">
-            <img src={qg3} className="img-fluid" alt="quick-guide" />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col text-center">
-            <img src={qg4} className="img-fluid" alt="quick-guide" />
-          </div>
+        <div>
+          <Document file={qg} onLoadSuccess={this.onDocumentLoad}>
+            <div className="row">
+              <div className="col text-center d-inline-block img-fluid">
+                <Page pageNumber={1} className={"qg"} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col text-center d-inline-block img-fluid">
+                <Page pageNumber={2} className={"qg"} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col text-center d-inline-block img-fluid">
+                <Page pageNumber={3} className={"qg"} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col text-center d-inline-block img-fluid">
+                <Page pageNumber={4} className={"qg"} />
+              </div>
+            </div>
+          </Document>
         </div>
 
         <Footer bgColor="#224394" fontColor="#fafafa" logo={logo_white} />
